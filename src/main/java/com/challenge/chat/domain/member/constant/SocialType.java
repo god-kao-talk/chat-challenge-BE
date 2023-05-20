f6fda2f3caf11fbc846229ba4fd594b3d2a84192
kimgun95 <harrison1995@naver.com>
2023-05-21 00:34:20 +0900

    feat : Member 도메인 추가 소셜 로그인을 고려하여 Member 엔티티 필드 구성 - SocialType은 다양한 소셜 로그인을 고려하여 추가함 - MemberRole은 jwt 구현시 authority에 대한 값 주입시 필요하여 추가
    Member 테이블에 대한 db 접근을 위해 MemberRepository 추가

---
+++ src/main/java/com/challenge/chat/domain/member/constant/SocialType.java
@@ -0,0 +1,5 @@
+package com.challenge.chat.domain.member.constant;
+
+public enum SocialType {
+	KAKAO, NAVER, GOOGLE
+}
---
+++ src/main/java/com/challenge/chat/domain/member/repository/MemberRepository.java
@@ -0,0 +1,25 @@
+package com.challenge.chat.domain.member.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.challenge.chat.domain.member.constant.SocialType;
+import com.challenge.chat.domain.member.entity.Member;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+	Optional<Member> findByEmail(String email);
+
+	Optional<Member> findByNickname(String nickname);
+
+	Optional<Member> findByRefreshToken(String refreshToken);
+
+	/**
+	 * 소셜 타입과 소셜의 식별값으로 회원 찾는 메소드
+	 * 정보 제공을 동의한 순간 DB에 저장해야하지만, 아직 추가 정보(사는 도시, 나이 등)를 입력받지 않았으므로
+	 * 유저 객체는 DB에 있지만, 추가 정보가 빠진 상태이다.
+	 * 따라서 추가 정보를 입력받아 회원 가입을 진행할 때 소셜 타입, 식별자로 해당 회원을 찾기 위한 메소드
+	 */
+	Optional<Member> findBySocialTypeAndSocialId(SocialType socialType, String socialId);
+}
