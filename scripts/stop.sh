b4bd2a02d6b2dd290e327ef1314b3b11447c6d29
Murphy <111736036+taekk1a2a3a@users.noreply.github.com>
2023-05-25 00:30:28 +0900

    fix : stop.sh 오류 수정
    start.sh 랑 같은 내용 오류 있었습니다 죄송합니다 !!

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,18 +2,16 @@
 
 ROOT_PATH="/home/ubuntu/spring-github-action"
 JAR="$ROOT_PATH/application-plain.jar"
-
-APP_LOG="$ROOT_PATH/application.log"
-ERROR_LOG="$ROOT_PATH/error.log"
-START_LOG="$ROOT_PATH/start.log"
+STOP_LOG="$ROOT_PATH/stop.log"
+SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
 
 NOW=$(date +%c)
 
-echo "[$NOW] $JAR 복사" >> $START_LOG
-cp $ROOT_PATH/build/libs/chat-0.0.1-SNAPSHOT.jar $JAR
 
-echo "[$NOW] > $JAR 실행" >> $START_LOG
-nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
-
-SERVICE_PID=$(pgrep -f $JAR)
-echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
+if [ -z "$SERVICE_PID" ]; then
+  echo " [$NOW] 서비스 NouFound" >> $STOP_LOG
+else
+  echo " [$NOW] [$SERVICE_PID] 서비스 종료 " >> $STOP_LOG
+  kill "$SERVICE_PID"
+  # kill -9 $SERVICE_PID # 강제 종료를 하고 싶다면 이 명령어 사용
+fi
