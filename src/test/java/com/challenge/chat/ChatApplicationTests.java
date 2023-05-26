24ab5460690e845fb31e6df7f22200e7d52757b6
rnjswo9578 <rnjswo153486@gmail.com>
2023-05-26 22:01:57 +0900

    Test : ChatService 성공 케이스 작성

--- src/test/java/com/challenge/chat/ChatApplicationTests.java
+++ src/test/java/com/challenge/chat/ChatApplicationTests.java
@@ -1,13 +1,16 @@
 package com.challenge.chat;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles({"test"})
 class ChatApplicationTests {
-
 	@Test
+	@DisplayName("통합 테스트 성공")
 	void contextLoads() {
 	}
-
 }
