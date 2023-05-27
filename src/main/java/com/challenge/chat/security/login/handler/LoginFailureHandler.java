8f5a36e5349f756574da5497b3fbf0cd593af4ed
rnjswo9578 <rnjswo153486@gmail.com>
2023-05-28 00:12:23 +0900

    feat : 일반 로그인 기능 추가(ignore 였던 파일)

---
+++ src/main/java/com/challenge/chat/security/login/handler/LoginFailureHandler.java
@@ -0,0 +1,28 @@
+package com.challenge.chat.security.login.handler;
+
+import java.io.IOException;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * JWT 로그인 실패 시 처리하는 핸들러
+ * SimpleUrlAuthenticationFailureHandler를 상속받아서 구현
+ */
+@Slf4j
+public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+        AuthenticationException exception) throws IOException {
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/plain;charset=UTF-8");
+        response.getWriter().write("로그인 실패! 이메일이나 비밀번호를 확인해주세요.");
+        log.info("로그인에 실패했습니다. 메시지 : {}", exception.getMessage());
+    }
+}
