12977aaca0b12046fdace0dfc60ad30bd267e667
rnjswo9578 <rnjswo153486@gmail.com>
2023-05-31 17:04:58 +0900

    refactor : JwtAuthenticationProcessingFilter = 회원가입 시 작동 안되게 조건 추가

--- src/main/java/com/challenge/chat/security/jwt/filter/JwtAuthenticationProcessingFilter.java
+++ src/main/java/com/challenge/chat/security/jwt/filter/JwtAuthenticationProcessingFilter.java
@@ -39,7 +39,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
 
-	private static final String NO_CHECK_URL = "/login"; // "/login"으로 들어오는 요청은 Filter 작동 X
+	private static final String NO_CHECK_URL_LOGIN = "/login"; // "/login"으로 들어오는 요청은 Filter 작동 X
+	private static final String NO_CHECK_URL_SIGNUP = "/users/signup"; // "/login"으로 들어오는 요청은 Filter 작동 X
 
 	private final JwtService jwtService;
 	private final MemberRepository memberRepository;
@@ -52,7 +53,7 @@ public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
 		IOException,
 		ServletException {
 
-		if (request.getRequestURI().equals(NO_CHECK_URL)) {
+		if (request.getRequestURI().equals(NO_CHECK_URL_LOGIN) || request.getRequestURI().equals(NO_CHECK_URL_SIGNUP)) {
 			filterChain.doFilter(request, response); // "/login" 요청이 들어오면, 다음 필터 호출
 			return; // return으로 이후 현재 필터 진행 막기 (안해주면 아래로 내려가서 계속 필터 진행시킴)
 		}
