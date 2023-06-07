cfb91ad556bd6747eb6e476612bc4281cd8d0006
gun <54833128+kimgun95@users.noreply.github.com>
2023-06-07 20:01:58 +0900

    refactor : 채팅방 생성시 사용되는 DTO 생성 기존에 ChatRoomDto를 파라미터로 이용했었음 채팅방 생성 요청시 전달하는 값은 roomName 밖에 없지만 ChatRoomDto는 그 밖에 다른 필드 값을 갖고 있음 이는 request DTO로써 사용하면서 헷갈릴 수 있는 여지가 있기 때문에 리팩토링

--- src/main/java/com/challenge/chat/domain/chat/dto/ChatRoomDto.java
+++ src/main/java/com/challenge/chat/domain/chat/dto/ChatRoomDto.java
@@ -1,5 +1,6 @@
 package com.challenge.chat.domain.chat.dto;
 
+import com.challenge.chat.domain.chat.dto.request.ChatRoomRequest;
 import com.challenge.chat.domain.chat.entity.ChatRoom;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,6 +17,14 @@ public class ChatRoomDto {
 	private String id;
 	private String roomId;
 	private String roomName;
+
+	private ChatRoomDto(String roomName) {
+		this.roomName = roomName;
+	}
+
+	public static ChatRoomDto from(ChatRoomRequest request) {
+		return new ChatRoomDto(request.getRoomName());
+	}
 
 	public static ChatRoomDto from(ChatRoom chatRoom) {
 		return new ChatRoomDto(chatRoom.getId(), chatRoom.getRoomId(), chatRoom.getRoomName());
