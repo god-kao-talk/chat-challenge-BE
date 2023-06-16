3b87befd9a42ac710ef829dd2c4ebb65c282267d
rnjswo9578 <rnjswo153486@gmail.com>
2023-06-16 11:21:56 +0900

    refactor : 레포지토리를 카산드라로 변경

--- src/main/java/com/challenge/chat/domain/chat/repository/ChatRepository.java
+++ src/main/java/com/challenge/chat/domain/chat/repository/ChatRepository.java
@@ -1,13 +1,11 @@
 package com.challenge.chat.domain.chat.repository;
 
 import com.challenge.chat.domain.chat.entity.Chat;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.cassandra.repository.CassandraRepository;
 
 import java.util.List;
 
-public interface ChatRepository extends MongoRepository<Chat, String> {
-    // Spring Data MongoDB -> https://docs.spring.io/spring-data/mongodb/docs/current/reference/html/
+public interface ChatRepository extends CassandraRepository<Chat, Long> {
 
     List<Chat> findByRoomId(String roomId);
 }
--- src/main/java/com/challenge/chat/domain/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/challenge/chat/domain/chat/repository/ChatRoomRepository.java
@@ -1,11 +1,13 @@
 package com.challenge.chat.domain.chat.repository;
 
 import com.challenge.chat.domain.chat.entity.ChatRoom;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.cassandra.repository.CassandraRepository;
 
+import java.util.List;
 import java.util.Optional;
-public interface ChatRoomRepository extends MongoRepository<ChatRoom, String> {
+
+public interface ChatRoomRepository extends CassandraRepository<ChatRoom, String> {
 
 	Optional<ChatRoom> findByRoomId(String roomId);
-
+	List<ChatRoom> findByRoomIdIn(List<String> roomIds);
 }
