d6688065a8db5b1f2927c572bdbc19947f772faf
eoneee <sangeon0412@gmail.com>
2023-06-23 17:31:52 +0900

    fix : test를 위한 ES주석처리

--- src/main/java/com/challenge/chat/config/ElasticSearchConfig.java
+++ src/main/java/com/challenge/chat/config/ElasticSearchConfig.java
@@ -1,21 +1,21 @@
-package com.challenge.chat.config;
-
-import org.elasticsearch.client.RestHighLevelClient;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.client.ClientConfiguration;
-import org.springframework.data.elasticsearch.client.RestClients;
-import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-
-@Configuration
-@EnableElasticsearchRepositories
-public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
-	@Override
-	public RestHighLevelClient elasticsearchClient() {
-		// http port 와 통신할 주소
-		ClientConfiguration configuration = ClientConfiguration.builder()
-			.connectedTo("localhost:9200")
-			.build();
-		return RestClients.create(configuration).rest();
-	}
-}
+//package com.challenge.chat.config;
+//
+//import org.elasticsearch.client.RestHighLevelClient;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.elasticsearch.client.ClientConfiguration;
+//import org.springframework.data.elasticsearch.client.RestClients;
+//import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+//import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+//
+//@Configuration
+//@EnableElasticsearchRepositories
+//public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
+//	@Override
+//	public RestHighLevelClient elasticsearchClient() {
+//		// http port 와 통신할 주소
+//		ClientConfiguration configuration = ClientConfiguration.builder()
+//			.connectedTo("localhost:9200")
+//			.build();
+//		return RestClients.create(configuration).rest();
+//	}
+//}
